b6ec1f363ae974b6eaf12dc7c35db7455f326c6e
Gowthami <pmpgowthami2k4@gmail.com>
2026-02-13 12:23:08 +0530

    UC5: Implemented menu-driven system to support multiple contacts

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -10,6 +10,50 @@ namespace AddressBookSystem
 
             AddressBook addressBook = new AddressBook();
 
+            while (true)
+            {
+                Console.WriteLine("\n===== MENU =====");
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Edit Contact");
+                Console.WriteLine("3. Delete Contact");
+                Console.WriteLine("4. Display All Contacts");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddContactFlow(addressBook);
+                        break;
+
+                    case "2":
+                        Console.Write("Enter first name to edit: ");
+                        addressBook.EditContact(Console.ReadLine());
+                        break;
+
+                    case "3":
+                        Console.Write("Enter first name to delete: ");
+                        addressBook.DeleteContact(Console.ReadLine());
+                        break;
+
+                    case "4":
+                        addressBook.DisplayAllContacts();
+                        break;
+
+                    case "5":
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+        }
+
+        private static void AddContactFlow(AddressBook addressBook)
+        {
             Console.WriteLine("\nEnter Contact Details:");
 
             Console.Write("First Name: ");
@@ -48,26 +92,8 @@ namespace AddressBookSystem
             );
 
             addressBook.AddContact(contact);
-
-            Console.WriteLine("\nAll Contacts:");
-            addressBook.DisplayAllContacts();
-
-            Console.Write("\nEnter first name to edit: ");
-            string nameToEdit = Console.ReadLine();
-
-            addressBook.EditContact(nameToEdit);
-
-            Console.WriteLine("\nUpdated Contact List:");
-            addressBook.DisplayAllContacts();
-
-            Console.Write("\nEnter first name to delete: ");
-            string nameToDelete = Console.ReadLine();
-
-            addressBook.DeleteContact(nameToDelete);
-
-            Console.WriteLine("\nUpdated Contact List:");
-            addressBook.DisplayAllContacts();
-
         }
+
+
     }
 }
