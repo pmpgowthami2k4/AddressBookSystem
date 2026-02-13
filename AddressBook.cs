ca8fc94eaf72bd677cfbcf6f0d5d99444f7d29e1
Gowthami <pmpgowthami2k4@gmail.com>
2026-02-13 11:26:45 +0530

    UC1: Created Contact class with required fields

---
+++ AddressBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    internal class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+
+            Contact contact = new Contact(
+                "Gowthami",
+                "Payasam",
+                "123 Main Street",
+                "Hyderabad",
+                "Telangana",
+                "500001",
+                "9876543210",
+                "gowthami@email.com"
+            );
+
+            Console.WriteLine("\nContact Created Successfully:\n");
+            Console.WriteLine(contact);
+        }
+    }
+}
---
+++ contact.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    internal class Contact
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Address { get; }
+        public string City { get; }
+        public string State { get; }
+        public string Zip { get; }
+        public string PhoneNumber { get; }
+        public string Email { get; }
+
+        public Contact(string firstName,
+                       string lastName,
+                       string address,
+                       string city,
+                       string state,
+                       string zip,
+                       string phoneNumber,
+                       string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            Zip = zip;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}\n" +
+                   $"Address: {Address}\n" +
+                   $"City: {City}\n" +
+                   $"State: {State}\n" +
+                   $"Zip: {Zip}\n" +
+                   $"Phone: {PhoneNumber}\n" +
+                   $"Email: {Email}";
+        }
+    }
+}
+
